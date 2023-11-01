caf24dbb508e7008a1d5dcd09feb15bf33f4f498
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2023-11-01 23:51:19 +0530

    Odd Even

---
+++ Others/oddEven.js
@@ -0,0 +1,30 @@
+function runProgram(input) {
+    let n = +input;
+    let isEven = n % 2 === 0;
+    if(isEven){
+        console.log('Even');
+    }else{
+        console.log('Odd');
+    }
+}
+  
+  if (process.env.USER === 'dilshadahmad') {
+    runProgram(`3`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
