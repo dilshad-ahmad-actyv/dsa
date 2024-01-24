0d3a304fede332b24f5d2759d65482c61e8ddbc9
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-01-25 00:58:16 +0530

    desired star pattern

--- Patterns/starCompound.js
+++ Patterns/starCompound.js
@@ -1,22 +1,95 @@
-let n = 5;
-let mid = Math.floor(n/2); 
-console.log(mid)
+let n = 6;
+let mid = Math.floor(n/2);
+let start = 0;
+let end = n  - 1;
 for(let i=0; i<n; i++){
     let str = '';
     for(let j=0; j<n; j++){
-        
-        if(i === mid && j === mid){
-            str +=  ' ' + ' ';
-        }else{
+        if(i == 0 || j == 0 || i ==n-1 || j == n-1){
             str += '*' + ' ';
+        }else{
+            if(i == start + 1 || j == start + 1 || i == end - 1 || j == end-1){
+                str += ' ' + ' ';
+            }else{
+               str += '*' + ' ';
+                
+            }
+            
         }
 
-        // if(i === mid || j === mid){
-        //     str +=  ' ' + ' ';
-        // }else{
-        //     str += '*' + ' ';
-        // }
 
     }
     console.log(str.trim());
 }
+
+
+
+// let n = 10;
+// let mid = Math.floor(n/2);
+// let start = mid - 1;
+// let end = n - mid;
+// for(let i=0; i<n; i++){
+//     let str = '';
+//     for(let j=0; j<n; j++){
+//         if(i == 0 || j == 0 || i ==n-1 || j == n-1){
+//             str += '*' + ' ';
+//         }else{
+//             if(i == start + 1 || j == start + 1 || i == end - 1 || j == end-1){
+//                 str += ' ' + ' ';
+//             }else{
+//                str += '*' + ' ';
+                
+//             }
+            
+//         }
+
+
+//     }
+//     console.log(str.trim());
+// }
+
+
+// * * * * * * * * * *
+// *                 *
+// *   * * * * * *   *
+// *   * * * * * *   *
+// *   * * * * * *   *
+// *   * * * * * *   *
+// *   * * * * * *   *
+// *   * * * * * *   *
+// *                 *
+// * * * * * * * * * *
+
+
+
+
+
+
+
+
+
+
+
+// let n = 10;
+// let mid = Math.floor(n/2); 
+// for(let i=0; i<n; i++){
+//     let str = '';
+//     for(let j=0; j<n; j++){
+//         if(i == 0 || j == 0 || i ==n-1 || j == n-1){
+//             str += '*' + ' ';
+//         }else{
+//             if( ((i === mid && i !==j) || (j=== mid && i !==j))){
+//                 str += '*' + ' ';
+//             }else{
+//                 str +=  ' ' + ' ';
+//             }
+            
+//         }
+
+
+//     }
+//     console.log(str.trim());
+// }
+
+
+
