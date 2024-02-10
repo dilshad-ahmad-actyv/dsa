426ef57eca11fb015008b2dff4a964a8e86d5e84
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-02-10 16:48:38 +0530

    sort: insertion sort algo

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,15 @@
+let arr = [5,5, 4, 5, 4, 4, 4, 3, 1,1,1, 2, 1];
+let n = arr.length;
+
+for(let i=1; i<n; i++){
+    let current = arr[i];
+    let j = i-1;
+
+    while(j >=0 && current < arr[j]){
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = current;
+}
+
+console.log(arr)
