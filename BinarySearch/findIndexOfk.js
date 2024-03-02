ddf20a62ea409f96c5a2db4f2e39e1ad6fca1bd1
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-03-02 18:42:51 +0530

    binary search: find index of k recursively

--- BinarySearch/findIndexOfk.js
+++ BinarySearch/findIndexOfk.js
@@ -1,24 +1,54 @@
 const arr = [1, 3, 4, 6, 7, 10, 20, 23, 34, 40, 42, 45, 50];
-const k = 23;
+const k = 1000;
 
-function findIndexOfK(arr, k) {
+// iterative method
+// function findIndexOfK(arr, k) {
+//   let l = 0;
+//   let n = arr.length;
+//   let h = n - 1;
+//   while (l <= h) {
+//     let mid = Math.floor((l + h) / 2);
+//     if (k === arr[mid]) {
+//       return mid;
+//     }
+
+//     if (k < arr[mid]) {
+//       h = mid - 1;
+//     } else {
+//       l = mid + 1;
+//     }
+//   }
+//   return -1;
+// }
+
+// const result = findIndexOfK(arr, k);
+// console.log(result);
+
+// recursive method
+
+function RfindIndexOfK(arr, k) {
   let l = 0;
   let n = arr.length;
   let h = n - 1;
-  while (l <= h) {
-    let mid = Math.floor((l + h) / 2);
-    if (k === arr[mid]) {
-      return mid;
-    }
-
-    if (k < arr[mid]) {
-      h = mid - 1;
+  function findIndex(arr, k, l, h) {
+    console.log(k, l, h);
+    if (l === h) {
+      if (arr[l] === k) {
+        return l;
+      } else {
+        return -1;
+      }
     } else {
-      l = mid + 1;
+      let mid = Math.floor((l + h) / 2);
+      if (arr[mid] > k) {
+        return findIndex(arr, k, l, mid - 1);
+      } else {
+        return findIndex(arr, k, mid + 1, h);
+      }
     }
   }
-  return -1;
+  return findIndex(arr, k, l, h);
 }
 
-const result = findIndexOfK(arr, k);
+const result = RfindIndexOfK(arr, k);
 console.log(result);
