1199eac33e42e7a0ff05b6f0ad0bcc87c2138807
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-08-02 23:02:34 +0530

    triangle pattern solved: all four side

--- String/patterns/triangle/triangle.js
+++ String/patterns/triangle/triangle.js
@@ -1,12 +1,44 @@
-const n = 5;
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let pattern = "";
+//   for (let j = 0; j < n - i - 1; j++) {
+//     pattern += "  ";
+//   }
 
-for (let i = 0; i < n; i++) {
-  let pattern = "";
-  for (let j = 0; j < i; j++) {
-    pattern += "   ";
-  }
-  for (let j = 0; j < n - i; j++) {
-    pattern += " * ";
-  }
-  console.log(pattern);
-}
+//   for (let k = 0; k < i + 1; k++) {
+//     pattern += " *";
+//   }
+//   console.log(pattern);
+// }
+
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   let pattern = "";
+//   for (let j = 0; j < i + 1; j++) {
+//     pattern += " *";
+//   }
+//   console.log(pattern);
+// }
+
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   let pattern = "";
+//   for (let j = 0; j < n - i; j++) {
+//     pattern += " *";
+//   }
+//   console.log(pattern);
+// }
+
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let pattern = "";
+//   for (let j = 0; j < i; j++) {
+//     pattern += "  ";
+//   }
+//   for (let k = 0; k < n - i; k++) {
+//     pattern += " *";
+//   }
+//   console.log(pattern);
+// }
