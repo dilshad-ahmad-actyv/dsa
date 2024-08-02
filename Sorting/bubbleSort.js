28eca3116732189eb4b9125ae4afb4f92d2632ee
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-08-03 00:29:34 +0530

    bubble sort algo solved

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,14 +1,25 @@
-let arr = [1, 2, 3, 4, 5, 6].reverse();
-let n = arr.length;
-console.log(arr)
-for(let i=0; i<n; i++){
-    for(let j=0; j<n - i - 1; j++){
-        if(arr[j] > arr[j+1]){
-            // let temp = arr[j];
-            // arr[j] = arr[j+1];
-            // arr[j+1] = temp;
-            [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
-        }
+// let arr = [1, 2, 3, 4, 5, 6].reverse();
+// let n = arr.length;
+// console.log(arr)
+// for(let i=0; i<n; i++){
+//     for(let j=0; j<n - i - 1; j++){
+//         if(arr[j] > arr[j+1]){
+//             // let temp = arr[j];
+//             // arr[j] = arr[j+1];
+//             // arr[j+1] = temp;
+//             [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+//         }
+//     }
+// }
+// console.log(arr)
+
+const arr = [2, 3, 1, 2, 2, 0, -1, 100, 0, 8, 4];
+const n = arr.length;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n - i; j++) {
+    if (arr[j] < arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
+  }
 }
-console.log(arr)
+console.log(arr);
