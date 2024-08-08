67d045fd9e66af75212b8e791e843c4782391fe0
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-08-09 00:04:29 +0530

    fibonacci with iterative method

--- DP/fibonacci.js
+++ DP/fibonacci.js
@@ -1,19 +1,36 @@
 // using recursive method
+// function fibonacci(n, memo) {
+//   if (n <= 1) return n;
+
+//   if (memo[n] !== -1) {
+//     return memo[n];
+//   }
+
+//   const fb1 = fibonacci(n - 2, memo);
+//   const fb2 = fibonacci(n - 1, memo);
+//   const fb = fb1 + fb2;
+//   memo[n] = fb;
+//   return memo[n];
+// }
+
+// const n = 10;
+// const memo = new Array(n + 1).fill(-1);
+// const answer = fibonacci(n, memo);
+// console.log(answer);
+
+// fibonacci with iterative/tabular method
+
 function fibonacci(n, memo) {
   if (n <= 1) return n;
+  memo[0] = 0;
+  memo[1] = 1;
 
-  if (memo[n] !== -1) {
-    return memo[n];
+  for (let i = 2; i <= n; i++) {
+    memo[i] = memo[i - 2] + memo[i - 1];
   }
-
-  const fb1 = fibonacci(n - 2, memo);
-  const fb2 = fibonacci(n - 1, memo);
-  const fb = fb1 + fb2;
-  memo[n] = fb;
   return memo[n];
 }
-
 const n = 10;
 const memo = new Array(n + 1).fill(-1);
-const answer = fibonacci(n, memo);
-console.log(answer);
+const ans = fibonacci(n, memo);
+console.log(ans)
