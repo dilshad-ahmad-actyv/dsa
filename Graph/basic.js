595c86875a546faced56cb7dd0045eb56d0e29b7
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-08-11 21:33:02 +0530

    breadthFirstPrint in graph

--- Graph/basic.js
+++ Graph/basic.js
@@ -11,13 +11,37 @@
 //   }
 // };
 
-const depthFirstPrint = (graph, source) => {
-  console.log(source);
+// const depthFirstPrint = (graph, source) => {
+//   console.log(source);
 
-  for (let neighbour of graph[source]) {
-    depthFirstPrint(graph, neighbour);
+//   for (let neighbour of graph[source]) {
+//     depthFirstPrint(graph, neighbour);
+//   }
+// };
+// const graph = {
+//   a: ["c", "b"],
+//   b: ["d"],
+//   c: ["e"],
+//   d: ["f"],
+//   e: [],
+//   f: [],
+// };
+
+// depthFirstPrint(graph, "a");
+
+const breadthFirstPrint = (graph, source) => {
+  const queue = [source];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    console.log(current);
+
+    for (let neighbour of graph[current]) {
+      queue.push(neighbour);
+    }
   }
 };
+
 const graph = {
   a: ["c", "b"],
   b: ["d"],
@@ -27,4 +51,4 @@ const graph = {
   f: [],
 };
 
-depthFirstPrint(graph, "a");
+breadthFirstPrint(graph, "a");
