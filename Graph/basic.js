a3912d52cbd222aea746951a58ceea8d43b89804
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-08-11 23:22:42 +0530

    created dynamic graph

--- Graph/basic.js
+++ Graph/basic.js
@@ -29,26 +29,63 @@
 
 // depthFirstPrint(graph, "a");
 
-const breadthFirstPrint = (graph, source) => {
-  const queue = [source];
+// const breadthFirstPrint = (graph, source) => {
+//   const queue = [source];
 
-  while (queue.length > 0) {
-    const current = queue.shift();
-    console.log(current);
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+//     console.log(current);
 
-    for (let neighbour of graph[current]) {
-      queue.push(neighbour);
-    }
-  }
-};
+//     for (let neighbour of graph[current]) {
+//       queue.push(neighbour);
+//     }
+//   }
+// };
 
-const graph = {
-  a: ["c", "b"],
-  b: ["d"],
-  c: ["e"],
-  d: ["f"],
-  e: [],
-  f: [],
-};
+// const graph = {
+//   a: ["c", "b"],
+//   b: ["d"],
+//   c: ["e"],
+//   d: ["f"],
+//   e: [],
+//   f: [],
+// };
 
-breadthFirstPrint(graph, "a");
+// breadthFirstPrint(graph, "a");
+
+
+// Creating graph dynamically
+
+// const graph = {};
+
+// function addNode(node) {
+//   if (!graph[node]) {
+//     graph[node] = [];
+//   }
+// }
+
+// function addEdge(node1, node2) {
+//   if (!graph[node1]) {
+//     addNode(node1);
+//   }
+
+//   if (!graph[node2]) {
+//     addNode(node2);
+//   }
+//   graph[node1].push(node2);
+// }
+
+// addNode("a");
+// addNode("b");
+// addNode("c");
+// addNode("d");
+// addNode("e");
+// addNode("f");
+
+// addEdge("a", "c");
+// addEdge("a", "b");
+// addEdge("b", "d");
+// addEdge("c", "e");
+// addEdge("d", "f");
+
+// console.log(graph)
