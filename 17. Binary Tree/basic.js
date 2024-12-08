82b6f8f46b05562761be3a801007aa3dd9d81028
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-12-08 12:48:46 +0530

    Tree: DFS and BFS

--- 17. Binary Tree/basic.js
+++ 17. Binary Tree/basic.js
@@ -2,26 +2,26 @@
 // 1. At most two children per node
 // 2. exactly one root node
 // 3. exactly one path b/w root node and any node
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
-const a = new Node(5);
-const b = new Node(11);
-const c = new Node(3);
-const d = new Node(4);
-const e = new Node(2);
-const f = new Node(1);
+// const a = new Node(5);
+// const b = new Node(11);
+// const c = new Node(3);
+// const d = new Node(4);
+// const e = new Node(2);
+// const f = new Node(1);
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
 
 //        a
 //      /   \
@@ -157,12 +157,82 @@ c.right = f;
 // const output = treeMinValue(a, Infinity);
 // console.log(output);
 
-const maxPathSum = (root) => {
-  if (!root) return -Infinity;
-  if (!root.left && !root.right) return root.val;
-  const maxChildVal = Math.max(maxPathSum(root.left), maxPathSum(root.right));
-  return root.val + maxChildVal;
-};
+// const maxPathSum = (root) => {
+//   if (!root) return -Infinity;
+//   if (!root.left && !root.right) return root.val;
+//   const maxChildVal = Math.max(maxPathSum(root.left), maxPathSum(root.right));
+//   return root.val + maxChildVal;
+// };
 
-const output = maxPathSum(a);
-console.log(output);
+// const output = maxPathSum(a);
+// console.log(output);
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+const e = new Node("E");
+const f = new Node("F");
+const g = new Node("G");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+
+// Depth-first search
+// A->B->D->E->C->F
+
+// function depthFirstValues(head){
+  //Iterative approach
+//   const stack = [head];
+//   const values = [];
+
+//   while(stack.length > 0){
+//     const currentNode = stack.pop();
+//     values.push(currentNode.value);
+
+//     if(currentNode.right) stack.push(currentNode.right)
+//     if(currentNode.left)stack.push(currentNode.left)
+//   }
+// return values;
+
+// Recursive approach
+//  if(!head) return [];
+//  const left = depthFirstValues(head.left);
+//  const right = depthFirstValues(head.right);
+//   return [head.value, ...left, ...right]
+// }
+
+// const result = depthFirstValues(a);
+// console.log(result);
+
+// Breadth-first search
+// A->B->C->D->E->F
+
+const breadthFirstValues = (head)=>{
+  const values = [];
+  const queue = [head];
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+    values.push(currentNode.value);
+
+    if (currentNode.left) queue.push(currentNode.left);
+    if (currentNode.right) queue.push(currentNode.right);
+  }
+  return values;
+}
+
+const result = breadthFirstValues(a)
+console.log(result)
